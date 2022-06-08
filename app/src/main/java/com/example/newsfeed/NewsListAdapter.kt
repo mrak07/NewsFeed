44184280b1ff15a44b9bb462e23d5eb7151b5b49
mrak07 <amitkumar962828@gmail.com>
2022-06-08 16:45:17 +0530

    API call

--- app/src/main/java/com/example/newsfeed/NewsListAdapter.kt
+++ app/src/main/java/com/example/newsfeed/NewsListAdapter.kt
@@ -1,45 +1,51 @@
 package com.example.newsfeed
 
 import android.view.LayoutInflater
-import android.view.OnReceiveContentListener
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class NewsListAdapter(private val items:ArrayList<String>, private val listener: NewsItemClicked): RecyclerView.Adapter<NewsViewHolders>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolders {
+class NewsListAdapter(private val listener: NewsItemClicked): RecyclerView.Adapter<NewsViewHolder>() {
 
-        val view=LayoutInflater.from(parent.context).inflate(R.layout.item_news,parent,false)
-        val viewHolder=NewsViewHolders(view)
+    private val items: ArrayList<News> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news, parent, false)
+        val viewHolder = NewsViewHolder(view)
         view.setOnClickListener{
-
-            listener.OnitemClicked(items[viewHolder.adapterPosition])
+            listener.onItemClicked(items[viewHolder.adapterPosition])
         }
         return viewHolder
-
-
-    }
-
-    override fun onBindViewHolder(holder: NewsViewHolders, position: Int) {
-        val currentItems=items[position]
-        holder.titleView.text=currentItems
-
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
 
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val currentItem = items[position]
+        holder.titleView.text = currentItem.title
+        holder.author.text = currentItem.author
+        Glide.with(holder.itemView.context).load(currentItem.imageUrl).into(holder.image)
+    }
+
+    fun updateNews(updatedNews: ArrayList<News>) {
+        items.clear()
+        items.addAll(updatedNews)
+
+        notifyDataSetChanged()
+    }
 }
 
-
-class  NewsViewHolders(itemView: View):RecyclerView.ViewHolder(itemView){
-    val titleView:TextView=itemView.findViewById(R.id.title);
+class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//    val titleView: TextView = itemView.findViewById(R.id.title)
+//    val image: ImageView = itemView.findViewById(R.id.image)
+//    val author: TextView = itemView.findViewById(R.id.author)
 }
 
-interface NewsItemClicked{
-        fun OnitemClicked(item:String ){
-
-        }
+interface NewsItemClicked {
+//    fun onItemClicked(item: News)
 }
